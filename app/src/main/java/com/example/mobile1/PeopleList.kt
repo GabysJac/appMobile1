0e357d394e327806adc4354cd79f32c866384532
Gabriela Farinango <gabriela.farinango@hotmail.com>
2024-03-22 20:53:14 -0500

    practica VIERNES 22 Manejo de hilos

--- app/src/main/java/com/example/mobile1/PeopleList.kt
+++ app/src/main/java/com/example/mobile1/PeopleList.kt
@@ -7,17 +7,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -25,12 +29,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 import java.util.UUID
 
 @Composable
 fun PersonList() {
+    var timeLeft by remember {
+        mutableIntStateOf(10)
+    }
+    LaunchedEffect(key1 = timeLeft) {
+        while (timeLeft>0){
+            delay(1000L)
+            timeLeft--
+        }
+    }
     var name by remember { mutableStateOf("") }
     var age by remember { mutableStateOf("") }
 
@@ -44,17 +60,62 @@ fun PersonList() {
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
+            val context= LocalContext.current
+            Text(
+                text = "Time left: ${timeLeft.toString()}",
+                modifier = Modifier
+                    .padding(16.dp),
+                fontSize = 20.sp
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                modifier = Modifier.padding(16.dp),
+                onClick = {
+                    var isPaused = true
+                }
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(20.dp),
+                    imageVector = Icons.Default.Warning,
+                    contentDescription = null
+                )
+            }
+
+            Button(
+                modifier = Modifier.padding(16.dp),
+                onClick = {
+                    timeLeft = 10
+                }
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(20.dp),
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = null
+                )
+            }
+
+
+
+
+
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ){
             TextField(
                 value = name,
                 placeholder = { Text("Ingrese nombre") },
                 onValueChange = { name = it },
                 modifier = Modifier.weight(1f)
             )
-
-
-
         }
-
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
